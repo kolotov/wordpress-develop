e136631c02c64ead4155d29987932228a20f6186
gkdev <gkolotov.adv@gmail.com>
2026-09-08 07:58:24 +0300

    Tests: Restore block template utils coverage

--- tests/phpunit/tests/Tests_Block_Template_Utils.php
+++ tests/phpunit/tests/Tests_Block_Template_Utils.php
@@ -200,7 +200,7 @@ class Tests_Block_Template_Utils extends WP_UnitTestCase {
 	 * @expectedDeprecated _inject_theme_attribute_in_block_template_content
 	 */
 	#[\PHPUnit\Framework\Attributes\Ticket( '59452' )]
-	#[\PHPUnit\Framework\Attributes\CoversNothing]
+	#[WP_PHPUnit_Covers( WP_PHPUnit_Covers::TARGET_FUNCTION, '_inject_theme_attribute_in_block_template_content' )]
 	public function test_inject_theme_attribute_in_block_template_content() {
 		$theme                           = get_stylesheet();
 		$content_without_theme_attribute = '<!-- wp:template-part {"slug":"header","align":"full", "tagName":"header","className":"site-header"} /-->';
